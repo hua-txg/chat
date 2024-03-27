9d5135fa5512a066fbcb22183763d4be3bafcc61
hua-txg <hua.txg@gmail.com>
2024-03-27 16:55:52 +0800

    encoding='utf-8-sig'

--- r1.py
+++ r1.py
@@ -2,10 +2,11 @@
 
 def read_file(filename):
 	lines = []
-	with open(filename, 'r', encoding='utf-8') as f:
+	with open(filename, 'r', encoding='utf-8-sig') as f:
 		for line in f:
 			lines.append(line.strip())
 	print(lines) 
+
 
 
 read_file('input.txt')
