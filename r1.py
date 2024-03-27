3128147eb90d4b4f4096f3125ffb8a379e4e5b78
hua-txg <hua.txg@gmail.com>
2024-03-27 16:57:45 +0800

    add  convert()

--- r1.py
+++ r1.py
@@ -5,8 +5,22 @@ def read_file(filename):
 	with open(filename, 'r', encoding='utf-8-sig') as f:
 		for line in f:
 			lines.append(line.strip())
-	print(lines) 
+	return lines 
 
+def convert(lines):
+	new = []
+	person = None
+	for line in lines:
+		if line == 'Allen':
+			person = 'Allen'
+			continue
+		elif line == 'Tom':
+			person = 'Tom'
+			continue
+		if person:
+			new.append(person + ': ' + line)
+	print(new) 
 
+lines = read_file('input.txt')
+lines = convert(lines)
 
-read_file('input.txt')
