35076a083d7a7a4d05a6e536f5d602ab62c8f94e
hua-txg <hua.txg@gmail.com>
2024-03-29 11:16:16 +0800

    import progressbar

--- reviews-import-time.py
+++ reviews-import-time.py
@@ -1,17 +1,20 @@
 import time
-
+import progressbar
 
 data = []
 count = 0
+bar = progressbar.ProgressBar(max_value=1000000)
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
-			print(len(data))
-print(len(data))
+		# if count % 1000 == 0: 
+		# 	print(len(data))   #每讀取1000筆就印出一次
+		bar.update(count)
 
-print(data[0])
+# print(len(data))
+
+# print(data[0])
 print('檔案讀取完了，總共有', len(data), '筆資料')
 
 #計算平均留言的長度，將每一筆留言的長度sum計算出來，除以留言筆數，就是留言的平均長度
@@ -41,7 +44,7 @@ good = [d for d in data if 'good' in d  ]
 print('list comprehension 清單快寫法 一共有', len(good), '筆留言提到good')
 
 bad = ['bad' in d for d in data]
-print(bad)
+# print(bad)
 
 
 #文字計數
@@ -63,7 +66,7 @@ for word in wc:
 
 end_time = time.time()
 print('花了', end_time - start_time, 'seconds')
-	
+
 	# print(wc)
 while True:
 	word = input('請問你想查什麼字：')
