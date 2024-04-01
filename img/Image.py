79f59a1f08a19a0f9c2440e0c4b9800db0cbaee2
hua-txg <hua.txg@gmail.com>
2024-04-01 17:28:39 +0800

    batch job  file orig convert file result

--- img/Image.py
+++ img/Image.py
@@ -1,4 +1,8 @@
 from PIL import Image
-image_file = Image.open('audi-A8-2.png') # open colour image
-image_file = image_file.convert('L') # convert image to black and white
-image_file.save('result-audi-A8-2.png')
+import os
+
+for file in os.listdir('orig'):
+	if file.endswith('.jpg'):
+		image_file = Image.open('orig/' + file) # open colour image
+		image_file = image_file.convert('L') # convert image to black and white
+		image_file.save('result/' + file[:-4] + '_grey.png')
